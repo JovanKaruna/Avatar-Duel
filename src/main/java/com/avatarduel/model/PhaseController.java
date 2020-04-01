f517f490e0e18a51901abb73434e2f9f87f8ff38
JovanKaruna <jovancahyadi@gmail.com>
2020-04-01 23:37:27 +0700

    added phase.fxml and phaseController.java

---
+++ src/main/java/com/avatarduel/model/PhaseController.java
@@ -0,0 +1,22 @@
+package com.avatarduel.model;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class PhaseController {
+
+    @FXML
+    private Text playerTurn;
+
+    @FXML
+    private Text turn;
+
+    @FXML
+    private Text phase;
+
+    @FXML
+    private Button next;
+
+}
+
